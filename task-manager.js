07a47010dd492fa002a25d2fbd9bcd6d2b0fdcc6
Sneha-Paul-coder <anthikkattsneha@gmail.com>
2026-05-18 01:18:46 +0530

    Initial commit

---
+++ task-manager.js
@@ -0,0 +1,78 @@
+const btn = document.getElementById("btn");
+const taskInput = document.getElementById("task-name");
+const taskListEl = document.getElementById("task-list");
+
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+function deleteTask(id) {
+  tasks = tasks.filter((task) => task.id !== id);
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  render();
+}
+
+function render() {
+  taskListEl.innerHTML = "";
+
+  tasks.forEach((task) => {
+    const li = document.createElement("li");
+
+    li.className =
+      "list-group-item d-flex  justify-content-between align-items-center";
+
+    const checkboxInput = document.createElement("input");
+    checkboxInput.type = "checkbox";
+    checkboxInput.className = "form-check-input me-1";
+    checkboxInput.checked = task.completed;
+
+    const label = document.createElement("label");
+    label.textContent = task.text;
+    label.className = "flex-grow-1 ms-2";
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "btn btn-danger btn-sm";
+
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", () => deleteTask(task.id));
+    checkboxInput.addEventListener("change", () => toggleTask(task.id));
+
+    li.appendChild(checkboxInput);
+    li.appendChild(label);
+    li.appendChild(deleteBtn);
+
+    taskListEl.appendChild(li);
+  });
+}
+
+function toggleTask(id) {
+  tasks = tasks.map((task) =>
+    task.id === id ? { ...task, completed: !task.completed } : task
+  );
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  render();
+}
+
+function addTaskBtn() {
+  const taskText = taskInput.value.trim();
+
+  if (!taskText) return;
+
+  tasks.push({
+    id: Date.now(),
+    text: taskText,
+    completed: false,
+  });
+
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  taskInput.value = "";
+
+  render();
+}
+
+btn.addEventListener("click", addTaskBtn);
+render();
